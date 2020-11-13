170eda3060c1aa041962503a6f48eff794e19b63
Noah Weiner <noah.weiner@yale.edu>
2020-11-13 13:53:21 -0500

    redirect output pt file

--- trace_model.py
+++ trace_model.py
@@ -22,6 +22,6 @@ types_to_quantize = {torch.nn.Conv1d, torch.nn.BatchNorm1d, torch.nn.ReLU}
 q = torch.quantization.quantize_dynamic(m, types_to_quantize, dtype=torch.qint8)
 
 s = torch.jit.script(q)
-torch.jit.save(s, "processed_mod.pt")
+torch.jit.save(s, "../processed_mod.pt")
 
 
